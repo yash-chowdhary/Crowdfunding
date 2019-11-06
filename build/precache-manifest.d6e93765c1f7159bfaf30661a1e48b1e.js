0d07022d4a5cb244bbda1f7d99033eafb9d46cbe
yash-chowdhary <yashchowdhary98@gmail.com>
2019-11-06 18:33:15 +0800

    Move folders to root directory

---
+++ build/precache-manifest.d6e93765c1f7159bfaf30661a1e48b1e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d078e2e62351dd1ff1d2bc9d91071757",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe9e670de2bddfc567d",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "fe773325da1781e2b404",
+    "url": "/static/css/main.9da61e51.chunk.css"
+  },
+  {
+    "revision": "efe9e670de2bddfc567d",
+    "url": "/static/js/2.d7a5bbfe.chunk.js"
+  },
+  {
+    "revision": "fe773325da1781e2b404",
+    "url": "/static/js/main.48a71536.chunk.js"
+  },
+  {
+    "revision": "9285894592c965babffb",
+    "url": "/static/js/runtime-main.91802079.js"
+  },
+  {
+    "revision": "889937b36dbf09756a9aea00936c5868",
+    "url": "/static/media/books.889937b3.jpg"
+  },
+  {
+    "revision": "377c17886cd443a1b05e7c09ae977a5c",
+    "url": "/static/media/fundit.377c1788.png"
+  },
+  {
+    "revision": "17cb381fdd13f864e462b326e4a096e9",
+    "url": "/static/media/game.17cb381f.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "4a2a39c8b60b2d0173b63779bbfa8cc6",
+    "url": "/static/media/movie.4a2a39c8.jpg"
+  }
+]);
